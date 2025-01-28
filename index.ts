2902807024ddce2d6d628adc650b1aa8043f1a8a
Dragodui <alghazaliaks123@gmail.com>
2025-01-28 10:37:38 +0100

    typescript

--- index.ts
+++ index.ts
@@ -1,21 +1,46 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import fs from "fs";
-import sqlite3 from 'sqlite3';
+import sqlite3, { Database } from 'sqlite3';
 import { open } from 'sqlite';
 
-const polishAlphabet = [
+interface Item {
+    item: string;
+}
+
+interface Classroom {
+    item: string;
+    building: string;
+}
+
+interface Event {
+    title: string;
+    start: string;
+    end: string;
+    description: string;
+    worker_title: string;
+    worker: string;
+    room: string;
+    group_name: string;
+    tok_name: string;
+    lesson_form: string;
+    lesson_form_short: string;
+    lesson_status: string;
+    color: string;
+}
+
+const polishAlphabet: string[] = [
     'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł',
     'M', 'N', 'Ń', 'O', 'Ó', 'P', 'R', 'S', 'Ś', 'T', 'U', 'W', 'Y', 'Z', 'Ź', 'Ż'
 ];
 
-const getItems = async (kind, filename) => {
-    let results = [];
+const getItems = async (kind: string, filename: string): Promise<void> => {
+    let results: Item[] = [];
 
     for (let letter of polishAlphabet) {
         try {
-            const apiUrl = `https://plan.zut.edu.pl/schedule.php?kind=${kind}&query=` + letter;
+            const apiUrl: string = `https://plan.zut.edu.pl/schedule.php?kind=${kind}&query=` + letter;
             console.log(apiUrl);
-            const response = await axios.get(apiUrl);
+            const response: AxiosResponse = await axios.get(apiUrl);
             let data = response.data;
 
             try {
@@ -33,7 +58,7 @@ const getItems = async (kind, filename) => {
                 results = [...results, ...filteredResults];
             }
         } catch (error) {
-            console.error("Error fetching data for letter:", letter, error.message);
+            console.error("Error fetching data for letter:", letter, error);
         }
     }
     const uniqueResults = results.filter((obj, index, self) =>
@@ -42,9 +67,9 @@ const getItems = async (kind, filename) => {
     fs.writeFileSync(filename, JSON.stringify(uniqueResults, null, 2));
 }
 
-const getSchedule = async (teachers) => {
+const getSchedule = async (teachers: Item[]): Promise<void> => {
 
-    let schedule = [];
+    let schedule:Event[] = [];
 
     for (let teacher of teachers) {
 
@@ -59,7 +84,7 @@ const getSchedule = async (teachers) => {
                     data = JSON.parse(data);
                 }
             } catch (parseError) {
-                console.error(`Warning: data for letter ${letter} is not valid JSON.`);
+                console.error(`Warning: data for teacher ${teacher} is not valid JSON.`);
                 console.log(`Received data: ${data.slice(0, 100)}...`);
                 continue;
             }
@@ -69,7 +94,7 @@ const getSchedule = async (teachers) => {
                 schedule = [...schedule, ...filteredSchedule];
             }
         } catch (error) {
-            console.error("Error fetching data for letter:", letter, error.message);
+            console.error("Error fetching data for teacher:", teacher, error);
         }
     }
 
@@ -77,47 +102,47 @@ const getSchedule = async (teachers) => {
     fs.writeFileSync("schedule.json", JSON.stringify(schedule, null, 2));
 };
 
-const getTeachers = async () => {
+const getTeachers = async (): Promise<void> => {
     await getItems("teacher", "teachers.json");
 };
 
 
-const getClassrooms = async () => {
+const getClassrooms = async (): Promise<void> => {
     await getItems("room", "classrooms.json");
 };
 
-const getSubjects = async () => {
+const getSubjects = async (): Promise<void> => {
     await getItems("subject", "subjects.json");
 };
 
-const getBuildings = async () => {
+const getBuildings = async (): Promise<void> => {
     try {
         const classrooms = JSON.parse(fs.readFileSync("classrooms.json", "utf-8"));
-        const buildings = classrooms.map(classroom => classroom.item.split(" ")[0]);
-        const objectBuildings = buildings.map(building => ({ item: building }));
-        const uniqueBuildings = objectBuildings.filter((obj, index, self) =>
-            index === self.findIndex((el) => el.item === obj.item)
+        const buildings = classrooms.map((classroom: Item) => classroom.item.split(" ")[0]);
+        const objectBuildings = buildings.map((building: string) => ({ item: building }));
+        const uniqueBuildings = objectBuildings.filter((obj: Item, index: number, self: Item[]) =>
+            index === self.findIndex((el: Item) => el.item === obj.item)
         );
         fs.writeFileSync("buildings.json", JSON.stringify(uniqueBuildings, null, 2));
     } catch (error) {
-        console.error("Error fetching data for buildings:", error.message);
+        console.error("Error fetching data for buildings:", error);
     }
 };
 
 // API have not enough groups info
-const getGroups = async () => {  
+const getGroups = async (): Promise<void> => {  
     await getItems("group", "groups.json");
 };
 
 const modifyClassrooms = async () => {
     try {
         const classrooms = JSON.parse(fs.readFileSync("classrooms.json", "utf-8"));
-        classrooms.forEach(classroom => {
+        classrooms.forEach((classroom: Classroom) => {
             classroom.building = classroom.item.split(" ")[0];
         });
         fs.writeFileSync("classrooms.json", JSON.stringify(classrooms, null, 2));
     } catch (error) {
-        console.error("Error fetching data for buildings:", error.message);
+        console.error("Error fetching data for buildings:", error);
     }
 };
 
@@ -136,45 +161,45 @@ const initializeDB = async () => {
         await db.exec("CREATE TABLE IF NOT EXISTS schedule (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, start TEXT, end TEXT,description TEXT, workerTitle TEXT, worker TEXT, room TEXT, groupName TEXT, tokName, lessonForm, lessonFormShort TEXT, lessonStatus TEXT, color TEXT)");
         return db;
     } catch (error) {
-        console.error("Error initializing DB:", error.message);
+        console.error("Error initializing DB:", error);
     }
 }
 
-const putDataIntoDB = async (filename, table, db) => {
+const putDataIntoDB = async (filename: string, table: string, db: any) => {
     try {
         const items = JSON.parse(fs.readFileSync(filename, "utf-8"));
         const batchSize = 300;
         for (let i = 0; i < items.length; i += batchSize) {
             const batch = items.slice(i, i + batchSize);
             const placeholders = batch.map(() => '(?)').join(', ');
-            const values = batch.flatMap(item => [item.item]);
+            const values = batch.flatMap((item: Item) => [item.item]);
             const sql = `INSERT INTO ${table} (item) VALUES ${placeholders}`;
             await db.run(sql, values);
         }
         console.log("Inserted", items.length, "items into", table);
     } catch (error) {
-        console.error("Error putting data into DB:", error.message);
+        console.error("Error putting data into DB:", error);
     }
 }
 
-const putClassroomsIntoDB = async (filename, table, db) => {
+const putClassroomsIntoDB = async (filename: string, table: string, db: any) => {
     try {
         const items = JSON.parse(fs.readFileSync(filename, "utf-8"));
         const batchSize = 300;
         for (let i = 0; i < items.length; i += batchSize) {
             const batch = items.slice(i, i + batchSize);
             const placeholders = batch.map(() => '(?, ?)').join(', ');
-            const values = batch.flatMap(item => [item.item, item.building]);
+            const values = batch.flatMap((item: Classroom) => [item.item, item.building]);
             const sql = `INSERT INTO ${table} (item, building) VALUES ${placeholders}`;
             await db.run(sql, values);
         }
         console.log("Inserted", items.length, "items into", table);
     } catch (error) {
-        console.error("Error putting data into DB:", error.message);
+        console.error("Error putting data into DB:", error);
     }
 }
 
-const putScheduleIntoDB = async (filename, table, db) => {
+const putScheduleIntoDB = async (filename: string, table: string, db: any) => {
     try {
         const items = JSON.parse(fs.readFileSync(filename, "utf-8"));
         const batchSize = 300;
@@ -183,7 +208,7 @@ const putScheduleIntoDB = async (filename, table, db) => {
             const batch = items.slice(i, i + batchSize);
 
             const placeholders = batch.map(() => '(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)').join(', ');
-            const values = batch.flatMap(item => [
+            const values = batch.flatMap((item: Event) => [
                 item.title,
                 item.start,
                 item.end,
@@ -210,7 +235,7 @@ const putScheduleIntoDB = async (filename, table, db) => {
 
         console.log("Inserted", items.length, "items into", table);
     } catch (error) {
-        console.error("Error putting data into DB:", error.message);
+        console.error("Error putting data into DB:", error);
     }
 };
 
